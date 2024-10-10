c915530de590c0084054c2111cd1170a508d07c2
ar-temio <artemiofedot17@gmail.com>
2024-10-10 12:51:44 +0300

    i dout no commit

---
+++ lesson-1/ansv.py
@@ -0,0 +1,11 @@
+print("hello,my name tema")
+print("my 17 old")
+print("i love papa,mam")
+#ALEK=PAPA
+print("my name alek")
+print("my 40 old")
+print("i love tema,mam")
+print("my name mam")
+print("my 43 old")
+print("i love tema,alek")
+
