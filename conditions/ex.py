7759201444e11dd8f1792fc5b5a8b79dd7ea3dac
ar-temio <artemiofedot17@gmail.com>
2024-10-19 18:13:29 +0300

    ?

---
+++ conditions/ex.py
@@ -0,0 +1,12 @@
+a = int(input("a = "))
+b = int(input("b = "))
+
+rezult = a + b  
+
+print(f'{a} -{b}')
+otvet = int(input('enter your answer = '))
+
+if otvet == rezult:
+    print("WIN!!!!!!!!!!!!!")
+else:
+    print("pipapypa")
