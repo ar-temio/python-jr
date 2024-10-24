31e4523c64ba984783aace70dd3787c4dd2addae
ar-temio <artemiofedot17@gmail.com>
2024-10-24 16:30:16 +0300

    conditions

---
+++ conditions/ec.py
@@ -0,0 +1,18 @@
+name = input('enter your name - ')
+if name == 'tema':
+    print(f'считать {name}')
+if name =='papa':
+    print(f' idi pospi {name}')
+
+a = int(input("a = "))
+b = int(input("a = "))
+rezult = a + b
+
+
+print(f'{a} - {b}')
+otvet = int(input('vvedite swoi otvet'))
+
+if otvet == rezult:
+    print("DDDDDDAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!!")
+else:
+    print("pipapypu")
