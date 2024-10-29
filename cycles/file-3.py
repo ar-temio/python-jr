d64fd7e0cd1642ec274cf832b5fad9221d8a333c
ar-temio <artemiofedot17@gmail.com>
2024-10-29 12:46:27 +0300

    same

---
+++ cycles/file-3.py
@@ -0,0 +1,6 @@
+import turtle
+t =  turtle.Pen()
+t.pencolor("red")
+for x in range(1000):
+    t.forward(x)
+    t.left(250)
---
+++ cycles/one.py
@@ -0,0 +1,47 @@
+# i = 0
+# while i < 100:
+#     print(i)
+#     i = i + 1
+
+#  i in range(100):
+#     print(i)
+
+#i = 0
+#while i < 10:
+   # if i == 4:
+      #  print("Tema HaraTema")
+   # print(i)
+   # i = i + 1
+
+# action = input("enter action - up or down ")
+# a = int(input("a - "))
+# b = int(input("b - "))
+# 
+# if action == "up":
+#     i = a
+#     while i < b:
+#         print(i)
+#         i = i + 1
+# 
+# if action == "down":
+#     i = b
+#     while i > a:
+#         print(i)
+#         i = i - 1
+
+
+
+name = input("enter your name - ")
+if name == "Tema":
+    i = 10
+    while i < 20:
+        print(i)
+        i = i + 1
+if name == "papa":
+    i = 100
+    while i > 1:
+        print(i)
+        i = i - 1
+
+
+
