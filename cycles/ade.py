2842a0729a7a4ae9c174b01409b9cd5453c31b52
ar-temio <artemiofedot17@gmail.com>
2024-10-29 18:46:22 +0300

    same

---
+++ cycles/ade.py
@@ -0,0 +1,11 @@
+name = input("enter your name - ")
+if name =="Tema":
+    i = 0
+    while i < 100:
+        print(i)
+        i = i + 1
+if name == "papa":
+    i = 100
+    while i > 1:
+        print(i)
+        i = i - 1
