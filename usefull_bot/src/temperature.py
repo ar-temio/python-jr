b79c5886439bedccb9257355c614278ee27cbf6e
alex.fedotov <aleksei.fedotov@winfinity.live>
2024-11-16 16:53:18 +0300

    add windspeed

--- usefull_bot/src/temperature.py
+++ usefull_bot/src/temperature.py
@@ -7,13 +7,10 @@ def get_temperature():
         response = requests.get("https://wttr.in/Volgograd?format=j1")
         data = response.json()
         temp_C = data['current_condition'][0]['temp_C']
+        windspeed = data['current_condition'][0]['windspeedKmph']
         #############
 
-        if int(temp_C) > 0:
-            return f"что так жарко: +{temp_C}°C"
-        else:
-            return f" что так холодно: {temp_C}°C"
-        return temp_C
+        return f"Температура в Волгограде: {temp_C}°C, скорость ветра: {windspeed} км/ч"
 
         #############
     except Exception as e:
