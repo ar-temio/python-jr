cb33b08169234af7f14f7bf7abd02ded66d3b165
ar-temio <artemiofedot17@gmail.com>
2024-11-23 13:08:13 +0300

    some)))

---
+++ random/ylis.py
@@ -0,0 +1,22 @@
+import random 
+import turtle
+t = turtle.Pen()
+turtle.bgcolor("black")
+colors = ["orange"]
+for n in range (50):
+    t.pencolor(random.choice(colors))
+    size = random.randint(10,40)
+x = random.randrange(-turtle.window_widch()//2,
+                    turtle.window_widch()//2)
+y = random.randrange(-turtle.window_height()//2,
+                    turtle.window_height()//2)
+
+
+    t.penup()
+    t.setpos(x,y)
+    t.pendown()
+for m in range(size):
+    t.forward(m*2)
+    t.left(91)
+
+
