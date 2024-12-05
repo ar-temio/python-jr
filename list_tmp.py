4c1fb51f25d3f0c2949b3ed78a39be2642bc3875
alex.fedotov <aleksei.fedotov@winfinity.live>
2024-12-05 18:56:07 +0300

    add list examples

---
+++ list_tmp.py
@@ -0,0 +1,32 @@
+# container = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape']
+# print(f"container: {container}")
+
+# for item in container:
+#     print(f"{item}")
+#
+# print(len(container))
+
+# for i in range(len(container)):
+#     if i == 3:
+#         container.pop(i)
+#
+# print(f"container: {container}")
+
+
+# container = []
+# print(f"container: {container}")
+#
+# item = 'apple'
+# container.append(item)
+# print(f"container: {container}")
+#
+# container.append('banana')
+# print(f"container: {container}")
+#
+# container.pop(0)
+# print(f"container: {container}")
+
+item = "pineapple"
+
+for i in range(len(item)):
+    print(f"{i} --> {item[i]}")
