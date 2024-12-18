39269312f91a263135556e05bcc883d7699417e2
ar-temio <artemiofedot17@gmail.com>
2024-12-18 19:33:46 +0300

    xz

--- my_file.py
+++ my_file.py
@@ -1,16 +1,40 @@
 fruits = ["яблоко","банан","апельсин"," киви"]
 
-fruits.append("манго")
+# fruits.append("манго")
+# 
+# 
+# fruits.sort()
+# 
+# 
+# print("Сортированные фрукты:")
+# for fruit in fruits:
+#     print(fruit)
+# 
+# 
+# first_ftuit = fruits[0]
+# print(f"Первый фрукт в списке: {first_ftuit}")
+
+container = ["молоко", "мороженное", "каша", "суп", "арбузик"]
+
+for a in container:
+    fruits.append(a)
+
+print(fruits)
+
+for i in range(len(fruits)):
+    if fruits[i] == "каша":
+        fruits.pop(i)
+    
+print(fruits)
 
 
-fruits.sort()
+# fruits.append("молоко")
+# fruits.append("мороженное")
+# fruits.append("каша")
+# fruits.append("суп")
+# fruits.append("арбузик")
 
-
-print("Сортированные фрукты:")
-for fruit in fruits:
-    print(fruit)
-
-
-first_ftuit = fruits[0]
-print(f"Первый фрукт в списке: {first_ftuit}")
-
+# for fruit in fruits:
+#     print(fruit)
+#     if fruit == "мороженное":
+#         print("КУПИ ПАПЕ")
